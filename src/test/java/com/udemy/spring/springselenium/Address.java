d8db113eb34f30c978394af40910dd3d15c11918
Testing Project <kanunulanga@gmail.com>
2022-05-17 08:35:53 +0200

    Spring boot

--- src/test/java/com/udemy/spring/springselenium/Address.java
+++ src/test/java/com/udemy/spring/springselenium/Address.java
@@ -1,19 +1,26 @@
 package com.udemy.spring.springselenium;
 
+import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Address {
-    //Getting the amount from the property file.
+
+@Autowired
+private Faker faker;
+
+    public String getStreet() {
+        return this.faker.address().streetAddress();
+    }
+}
+/*
+ //Getting the amount from the property file.
     @Value("${street}")
     private String street;
 
     /*public Address(){
         this.street="32 Hill Street,Randburg, 2194";
-    }*/
-
-    public String getStreet() {
-        return street;
     }
-}
+ */
